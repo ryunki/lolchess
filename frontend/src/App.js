2fb298fb09b4c327bd34f24a36fb9ca0e33a7c1a
ryunkishin <ryunkishin@gmail.com>
2024-01-12 14:22:44 +0900

    sort traits by activated and inactivated traits from high to low. including logic for AKALI champion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [championSelectedList, setChampionSelectedList] = useState({});
   // display all the traits of selected champions
   const [showAllTraits, setShowAllTraits] = useState({});
-
+  // display trait and its activation numbers
   const [displayActivation, setDisplayActivation] = useState([])
 
   // when a champion is clicked
@@ -97,31 +97,38 @@ function App() {
     setChampionSelectedList({})
     setShowAllTraits({})
     setDisplayActivation([])
+    prev_kda = []
+    prev_trueDamage = []
   }
+
 useEffect(()=>{
   const collectTraits = {}
-  // for adding up accumulated traits and sorting from high to low
+  // for adding up number of accumulated traits and sorting from high to low
   Object.entries(championSelectedList).forEach(([key,value])=>{
     value.forEach(item =>{
       collectTraits[item] = (collectTraits[item] || null) + 1 
     })
   })
-  // add activation limits to each traits
+  // add array of activation limits to each traits for display
   Object.entries(collectTraits).forEach(([key,value])=>{
     if(displayActivation[key]){
       collectTraits[key] = [collectTraits[key], displayActivation[key]]
     }
   })
-  // const entries = Object.entries(collectTraits);
-  // // Sort the array by values
-  // entries.sort((a, b) => b[1] - a[1]);
-  // // Convert back to object
-  // const sortedObject = Object.fromEntries(entries);
+  // sort the traits in order
+  const [sortedData, sortedInactivatedCategories] = sortTraits(collectTraits)
+  
+  // Convert back to object
+  const sortedObject = Object.fromEntries(sortedData);
+ 
   // // Update state with the sorted object
-  // setShowAllTraits(sortedObject);
-  setShowAllTraits(collectTraits);
-
+  setShowAllTraits(sortedObject);
+  // setShowAllTraits(collectTraits);
+  console.log(championSelectedList)
+  console.log(collectTraits)
+  console.log(sortedInactivatedCategories)
 },[championSelectedList, displayClickedChampion,displayActivation])
+
   return (
     <>
       {/* display all champions */}
@@ -141,5 +148,127 @@ useEffect(()=>{
     </>
   );
 }
+var prev_kda = []
+var prev_trueDamage = []
+function sortTraits(collectTraits) {
+  const sort = Object.entries(collectTraits);
+  const activatedCategories = [];
+  const inactivatedCategories = [];
+  const akali = {kda:[false,0],trueDamage:[false,0]}
+  sort.forEach((item) => {
+    // check if the the current trait equals or greater than any number of the activation array 
+    const activated = item[1][1].some((num) => {
+      // if kda or trueDamage is activated then set it true and its trait 
+      if(akali[item[0]] && item[1][0] >= num){
+        akali[item[0]][0] = true
+        akali[item[0]][1] = item[1][0]
+      }
+      return item[1][0] >= num
+    });
+    // group them by activated and inactivated
+    if (activated) {
+      activatedCategories.push(item);
+    } else {
+      inactivatedCategories.push(item);
+    }
+  });
+  // console.log("sort: ",sort)
+  // console.log("prev_kda and TD: ", prev_kda,prev_trueDamage)
+  // console.log("akali kda: ", akali.kda)
+  // console.log("akali TD: ", akali.trueDamage)
+  // if(sort)
+  // if both traits are activated
+  if (akali.kda[0] && akali.trueDamage[0]){
+    if (akali.kda[1] === akali.trueDamage[1]){
+      if(prev_kda < prev_trueDamage){
+        console.log(" kda was added lately so remove truedamage")
+        Object.fromEntries(activatedCategories).trueDamage[0] += -1
+      }
+      if(prev_kda > prev_trueDamage){
+        console.log(" TD was added lately so remove kda")
+        Object.fromEntries(activatedCategories).kda[0] += -1
+      }
+    }
+    // prev_kda = []
+    // prev_trueDamage = []
+    if(akali.kda[1] > akali.trueDamage[1]){
+      Object.fromEntries(activatedCategories).trueDamage[0] += -1
+      console.log("deduct 1 trueDamage first")
+    }
+    if(akali.kda[1] < akali.trueDamage[1]){
+      Object.fromEntries(activatedCategories).kda[0] += -1
+      console.log("deduct 1 kda first")
+    }
+    // if kda is activated only, deduct 1 from trueDamage
+  }else if (akali.kda[0] && !akali.trueDamage[0]){
+    console.log("deduct 1 trueDamage second")
+    // trueDamage may not exist at this point
+    if (Object.fromEntries(inactivatedCategories).trueDamage){
+      Object.fromEntries(inactivatedCategories).trueDamage[0] += -1
+    }
+    // if trueDamage is activated only, deduct 1 from kda
+  }else if (akali.trueDamage[0] && !akali.kda[0]){
+    console.log("deduct 1 kda second")
+    // kda may not exist at this point
+    if(Object.fromEntries(inactivatedCategories).kda){
+      Object.fromEntries(inactivatedCategories).kda[0] += -1
+    }
+  }
+  const traitDifference = []
+  // Sort inactivated categories by least differences to smallest activation number by high to low
+  const sortedInactivatedCategories = inactivatedCategories.sort((a, b) => {
+    const differenceA = Math.abs(a[1][0]  - a[1][1][0]);
+    const differenceB = Math.abs(b[1][0]  - b[1][1][0]);
+    // if (differenceA === 1 || differenceB === 1){
+    traitDifference[a[0]] = differenceA
+    traitDifference[b[0]] = differenceB
 
+    // Sort by least differences from low to high
+    if (differenceA !== differenceB) {
+      return differenceA - differenceB;
+    }
+    // If differences are equal, sort values by high to low
+    return b[1][0]- a[1][0];
+  });
+  console.log(traitDifference)
+  Object.entries(traitDifference).reduce((acc,[key,value])=>{
+    console.log(acc, key,value)
+    if(!acc[value]){
+      console.log("hey")
+      acc[value] = []
+    }
+    acc[value].push(key)
+    return acc
+  },{})
+  // console.log(groupedData)
+  // Sort activated categories by value from high to low
+  const sortedActivatedCategories = activatedCategories.sort((a, b) => {
+    // Prioritize categories with activation array length of 1 at the top
+    if (a[1][1].length === 1 && b[1][1].length !== 1) {
+      // put first item before the second item
+      return -1;
+    }
+    if (b[1][1].length === 1 && a[1][1].length !== 1) {
+      // put second item before the first item
+      return 1;
+    }
+    // sort values by high to low
+    return b[1][0] - a[1][0]
+  });
+
+  // Combine sorted activated and inactivated categories
+  const sortedData = sortedActivatedCategories.concat(sortedInactivatedCategories);
+  
+  sort.forEach((item,idx) =>{
+    if (item[0] === "kda"){
+      // prev_kda.push(item[1][0])
+      prev_kda =  item[1][0]
+    }
+    if (item[0] === "trueDamage"){
+      // prev_trueDamage.push(item[1][0])
+      prev_trueDamage=item[1][0]
+    }
+  })
+  return [sortedData, sortedInactivatedCategories]
+}
 export default App;
