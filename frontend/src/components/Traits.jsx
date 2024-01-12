54853af74a11fdf31b9e2f15db5402a6cd1bf43d
ryunkishin <ryunkishin@gmail.com>
2024-01-12 14:23:16 +0900

    highliting activated traits

--- frontend/src/components/Traits.jsx
+++ frontend/src/components/Traits.jsx
@@ -1,38 +1,47 @@
-
-const styles = {
-  color: 'red',
-};
+import './style.css'
 
 const Traits = ({ showAllTraits }) => {
-// console.log(showAllTraits)
 const displayActivation = (current, indicator) =>{
-  // console.log(value)
-  const res = indicator.map((item, idx)=>{
+  const res = indicator.map((item, idx, arr)=>{
+    // if the trait meets the any of the activation number it highlights
     if (item <= current){
-      return <text style={styles}>
+      return <div className = "highlight" key={idx}>
         {item}
-      </text>
+      </div>
     }else{
-      return <text> {item}</text>
+      // if the trait is not activated
+      if(current < arr[0]){
+        if(idx > 0){
+          // this prevent from showing rest of the activation number
+          return null
+        }
+        // this shows the first activation number 
+        return <div className = "" key={idx}> {item}</div>
+      }
+      // this shows all the activation numbers for activated traits
+      return <div className = "" key={idx}> {item}</div>
     }
   })
 
   return <>
-  {current} / {res}
+  {current} [ {res} ]
   </>
 }
   return (
     <>
     <h3>Selected Champions' Traits</h3>
-      <ul>
+      <ul className="horizontal-list">
         {showAllTraits &&
           Object.entries(showAllTraits).map(([key, value]) => {
             return (
-              <li key={key}>
-                {key}: {displayActivation(value[0], value[1])}
+              <li key={key} style={{display:"flex", margin:"5px", height:"20px"}} >
+                <div style={{display:"flex"}}>
+                  {key} {displayActivation(value[0], value[1])}
+                </div>
               </li>
             );
           })}
+        {/* {showAllTraits && traitsManipulation(showAllTraits)} */}
       </ul>
     </>
     
