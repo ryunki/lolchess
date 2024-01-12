a3cb352abdecfa84187e4cdd08da0982649265ee
ryunkishin <ryunkishin@gmail.com>
2024-01-12 21:57:18 +0900

    yes

--- frontend/src/components/SelectedChampions.jsx
+++ frontend/src/components/SelectedChampions.jsx
@@ -1,9 +1,11 @@
 
 
-const SelectedChampions = ({championSelectedList}) => {
+const SelectedChampions = ({championSelectedList, refreshHandler,goBackHandler}) => {
   return (
     <>
     <h3>Selected Champions ({Object.keys(championSelectedList).length})</h3>
+    <button onClick={()=>refreshHandler()}>Refresh</button>
+    {/* <button onClick={()=>goBackHandler()}>Go Back</button> */}
       <ul>
         {championSelectedList &&
           Object.keys(championSelectedList).map((item, idx) => <li key={idx}>{item}</li>)}
