27e0c36b2a17599357365420ae1eb1ebfa0d4cec
ryunkishin <ryunkishin@gmail.com>
2024-01-13 19:52:12 +0900

    refactor. removed one useEffect() to solve rendering issue (render twice)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,8 +19,6 @@ function App() {
   const [displayActivation, setDisplayActivation] = useState({});
   // display recommended champions for inactivated traits
   const [recommendChamp, setRecommendChamp] = useState({});
-  // stores the inactivated traits with differences, and the selected champions list
-  const [dataForRecommendation, setDataForRecommendation] = useState({});
   // for changing color of selected champion
   const [selectedChampion, setSelectedChampion] = useState([]);
 
@@ -33,7 +31,8 @@ function App() {
   };
 
   // Function to find traits for the selected champion in a given synergy type
-  const findTraits = (synergyType,champ,selectedChampionTraits,activation) => {
+  const findTraits = (synergyType,champ,selectedChampionTraits,activation
+  ) => {
     Object.keys(synergy[synergyType]).forEach((type) => {
       // look for selected champion in every trait
       if (synergy[synergyType][type].champs.includes(champ)) {
@@ -42,7 +41,7 @@ function App() {
         activation[type] = synergy[synergyType][type].activation;
       }
     });
-    return [selectedChampionTraits, activation]
+    return [selectedChampionTraits, activation];
   };
 
   const onClickHandler = (champ, idx) => {
@@ -56,14 +55,16 @@ function App() {
     // Find traits in origins
     findTraits('origins', champ, selectedChampionTraits, activation);
 
-    championSelectedList[champ] ? 
-      // if selected champion already exists in the list, remove it
-      removeHandler(champ) :
-      // if selected champion does not exist in the list, Add selected champions and traits to the list
-      setChampionSelectedList((prev) => ({ ...prev, [champ]: selectedChampionTraits }));
-      // Add new traits and their activation indicator to the previous traits
-      setDisplayActivation((prev) => ({ ...prev, ...activation }));
-
+    championSelectedList[champ]
+      ? // if selected champion already exists in the list, remove it
+        removeHandler(champ)
+      : // if selected champion does not exist in the list, Add selected champions and traits to the list
+        setChampionSelectedList((prev) => ({
+          ...prev,
+          [champ]: selectedChampionTraits,
+        }));
+    // Add new traits and their activation indicator to the previous traits
+    setDisplayActivation((prev) => ({ ...prev, ...activation }));
     // to display currently clicked champion
     setDisplayClickedChampion([champ, selectedChampionTraits]);
   };
@@ -76,7 +77,7 @@ function App() {
         delete updatedList[champ];
         return updatedList;
       }
-      return prev
+      return prev;
     });
   };
 
@@ -89,9 +90,8 @@ function App() {
     setShowAllTraits({});
     // delete champs for recommendation
     setRecommendChamp({});
-    setDataForRecommendation({});
-    setSelectedChampion([])
-    // setDisplayActivation({})
+    setSelectedChampion([]);
+    setDisplayActivation({});
     prev_kda = [];
     prev_trueDamage = [];
   };
@@ -106,51 +106,83 @@ function App() {
         collectTraits[trait] = (collectTraits[trait] || null) + 1;
       });
     });
-    // attach array of activation limits to corresponding traits for display
+    // attach array of activation indicator to corresponding traits
     Object.keys(collectTraits).forEach((trait) => {
       if (displayActivation[trait]) {
         collectTraits[trait] = [collectTraits[trait], displayActivation[trait]];
       }
     });
-    
-    // sort the traits in order, and bring inactivated traits list
+
+    // sort the traits in order, and get inactivated traits list
     const [sortedData, traitDifferenceList] = sortTraits(collectTraits);
 
     // Convert back to object
     const sortedObject = Object.fromEntries(sortedData);
 
-    // Update state with the sorted object to be displayed in Traits Component
+    // Update with the sorted object to be displayed in Traits Component
     setShowAllTraits(sortedObject);
+
+    // if there are champions in the list
     if (Object.keys(championSelectedList).length !== 0) {
-      setDataForRecommendation(traitDifferenceList);
+      // for storing the list of recommended champions
+      let recommendedChampionsList = [];
+      Object.entries(traitDifferenceList).forEach(([diff, arrayOftraits]) => {
+        arrayOftraits.forEach((trait) => {
+          findAllChampionsWithTrait('classes');
+          findAllChampionsWithTrait('origins');
+          function findAllChampionsWithTrait(type) {
+            // find all the champs of inactivated traits
+            Object.keys(synergy[type]).forEach((item) => {
+              if (trait === item) {
+                recommendedChampionsList = [...recommendedChampionsList,...synergy[type][item].champs,];
+              }
+            });
+          }
+        });
+      });
+      // store list of selected champions
+      let selectedChampions = [];
+      // extract the names only
+      const champList = Object.keys(championSelectedList);
+      champList.forEach((champ) => {
+        selectedChampions.push(champ);
+      });
+      // remove list of selected champions from the recommend list
+      const filteredChamps = recommendedChampionsList.filter(
+        (champ) => !selectedChampions.includes(champ)
+      );
+      // count how many traits to be activated for one champion
+      // by counting the duplicate names
+      const finalChampList = filteredChamps.reduce((counts, el) => {
+        counts[el] = (counts[el] || 0) + 1;
+        return counts;
+      }, {});
+      // sort them high to low
+      const sorted = Object.entries(finalChampList).sort((a, b) => {
+        return b[1] - a[1];
+      });
+      setRecommendChamp(sorted);
+    } else {
+      // if the clicked champion is the last in selected champions list, remove recommended champ
+      setRecommendChamp({});
     }
   }, [championSelectedList, displayClickedChampion, displayActivation]);
 
   return (
     <>
       {/* display all champions */}
-      <ChampionsList
-        champs={champs}
-        onClickHandler={onClickHandler}
-        selectedChampion={selectedChampion}
+      <ChampionsList champs={champs} onClickHandler={onClickHandler} selectedChampion={selectedChampion}
       />
       {/* <button onClick={()=>refreshHandler()}>Refresh</button> */}
       {/* display currently selected champion */}
       <CurrentChampion displayClickedChampion={displayClickedChampion} />
       {/* display selected list of champions */}
-      <SelectedChampions
-        championSelectedList={championSelectedList}
-        refreshHandler={refreshHandler}
+      <SelectedChampions championSelectedList={championSelectedList} refreshHandler={refreshHandler}
       />
       {/* display traits of all selected champions */}
       <Traits showAllTraits={showAllTraits} />
       {/* display recommended champions to activate traits */}
-      <Recommendation
-        dataForRecommendation={dataForRecommendation}
-        championSelectedList={championSelectedList}
-        recommendChamp={recommendChamp}
-        setRecommendChamp={setRecommendChamp}
-      />
+      <Recommendation recommendChamp={recommendChamp} />
     </>
   );
 }
@@ -220,7 +252,7 @@ function sortTraits(collectTraits) {
       inactivatedCat.kda[0] += -1;
     }
   }
-  
+
   // save AKALI's trait values for comparison
   Object.entries(collectTraits).forEach(([trait, value]) => {
     if (trait === 'kda') {
@@ -233,10 +265,10 @@ function sortTraits(collectTraits) {
 
   // this variable is for saving inactivated traits and re-arrange them by diffence as a key and name of traits as value
   let traitDifference = {};
-  let sortedInactivatedCategories = []
-  if(inactivatedCategories.length > 1){
+  let sortedInactivatedCategories = [];
+  if (inactivatedCategories.length > 1) {
     // Sort inactivated categories by least differences to smallest activation number by high to low
-     sortedInactivatedCategories = inactivatedCategories.sort((a, b) => {
+    sortedInactivatedCategories = inactivatedCategories.sort((a, b) => {
       // a[0] is the name of trait
       // a[1][0] is the current value of trait
       // aa[1][1][0] is the first number of activation indicator array
@@ -253,11 +285,11 @@ function sortTraits(collectTraits) {
       // If differences are equal, sort values by high to low
       return b[1][0] - a[1][0];
     });
-  } else if(inactivatedCategories.length === 1){
-    const cat = inactivatedCategories[0]
-    const difference = Math.abs(cat[1][0] - cat[1][1][0])
-    traitDifference[cat[0]] = difference
-    sortedInactivatedCategories = inactivatedCategories
+  } else if (inactivatedCategories.length === 1) {
+    const cat = inactivatedCategories[0];
+    const difference = Math.abs(cat[1][0] - cat[1][1][0]);
+    traitDifference[cat[0]] = difference;
+    sortedInactivatedCategories = inactivatedCategories;
   }
   // re-arrange the trait differences by {1:[name of traits], 2:[name of traits]}
   const traitDifferenceList = Object.entries(traitDifference).reduce(
@@ -286,7 +318,9 @@ function sortTraits(collectTraits) {
     return b[1][0] - a[1][0];
   });
   // Combine sorted activated and inactivated categories
-  const sortedData = sortedActivatedCategories.concat(sortedInactivatedCategories);
+  const sortedData = sortedActivatedCategories.concat(
+    sortedInactivatedCategories
+  );
   return [sortedData, traitDifferenceList];
 }
 export default App;
--- frontend/src/components/Recommendation.jsx
+++ frontend/src/components/Recommendation.jsx
@@ -1,57 +1,9 @@
 // @ts-nocheck
-import { useState, useEffect } from 'react';
-import {synergy } from '../constants';
 
-const Recommendation = ({dataForRecommendation, championSelectedList, recommendChamp, setRecommendChamp}) => {
+const Recommendation = ({ recommendChamp}) => {
   //  dataForRecommendation
   // 1: [trait1,trait2,trait3]
   // 2: [trait1,trait2,trait3]
-  useEffect(()=>{
-    const data = Object.entries(dataForRecommendation)
-    // 0: ['1' , [trait1,trait2]]
-    // 1: ['2' , [trait1,trait2]]
-      if(data.length > 0){
-        // for storing the list of champions
-        let recommendedChampionsList = []
-        // check if the inactivated trait exists
-        if(data[0][1]){
-          // loop through the champ list of inactivated traits with difference of 1
-          data[0][1].forEach(trait=>{
-            // find all the champions that meets the trait 
-            Object.keys(synergy.classes).forEach(clas => {
-              if(trait === clas ){
-                recommendedChampionsList = [...recommendedChampionsList, ...synergy.classes[clas].champs]
-              }
-            })
-            Object.keys(synergy.origins).forEach(orig => {
-              if(trait === orig ){
-                recommendedChampionsList = [...recommendedChampionsList, ...synergy.origins[orig].champs]
-              }
-            })
-          })
-        }
-        // store list of selected champions
-        let selectedChampions = []
-        // extract the names only
-        const champList = Object.keys(championSelectedList)
-        champList.forEach(champ =>{
-          selectedChampions.push(champ)
-        })
-        // remove list of selected champions from the recommend list
-        const filteredChamps = recommendedChampionsList.filter(champ => !selectedChampions.includes(champ))
-        // count how many traits to be activated for one champion
-        // by counting the duplicate names
-        const finalChampList = filteredChamps.reduce((counts,el)=>{
-          counts[el] = (counts[el] || 0) + 1
-          return counts
-        },{})
-        // sort them high to low 
-        const sorted = Object.entries(finalChampList).sort((a,b)=>{
-          return b[1] - a[1]
-        })
-        setRecommendChamp(sorted)
-
-}},[dataForRecommendation,setRecommendChamp,championSelectedList])
   
   return (<>
     <div>Recommendation</div>
