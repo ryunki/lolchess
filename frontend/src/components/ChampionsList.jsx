c2af3a4352f3dcb1665ced1ca23c2f843cdac0c1
ryunkishin <ryunkishin@gmail.com>
2024-01-15 02:11:08 +0900

    added colors by champion cost

--- frontend/src/components/ChampionsList.jsx
+++ frontend/src/components/ChampionsList.jsx
@@ -1,15 +1,20 @@
+import { useEffect, useState } from 'react'
 import '../css/ChampionsList.css'
 
 const ChampionsList = ({champs, onClickHandler,selectedChampion}) => {
+  // console.log(champs)
+  const costArray = ['','one', 'two', 'three', 'four', 'five']
+ 
   return (
     <>
     <h1>Champs List</h1>
     <div className='champions_list_wrapper'>
-        {champs.map((champ, idx) => (
-          <div  key = {idx} className = {`champion-item ${selectedChampion.includes(champ) ? 'selected' : ''}`} onClick={()=>onClickHandler(champ)}>
-              {champ}
+        {champs.map((champ, idx) => {
+      
+          return <div  key = {idx} className = {`champion-item blurred-edge ${costArray[champ[1]]} ${selectedChampion.includes(champ[0]) ? 'selected' : ''} `} onClick={()=>onClickHandler(champ[0])}>
+              {champ[0]}
             </div>
-        ))}
+})}
     </div>
     </>
   )
