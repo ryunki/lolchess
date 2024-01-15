227d8a27e3c0c500e2619543c3ae704f2786a180
ryunkishin <ryunkishin@gmail.com>
2024-01-15 15:53:36 +0900

    added cost indicators and search bar for displaying champions

--- frontend/src/components/ChampionsList.jsx
+++ frontend/src/components/ChampionsList.jsx
@@ -6,6 +6,7 @@ import '../css/ChampionsList.css';
 const ChampionsList = ({ onClickHandler, selectedChampion }) => {
   const [sortAtoZ, setSortAtoZ] = useState(true);
   const [sortCost, setSortCost] = useState(true);
+  // display all champions
   const [champions, setChampions] = useState(champs);
   // champion's cost array for displaying color
   const costArray = ['', 'one', 'two', 'three', 'four', 'five'];
@@ -13,22 +14,32 @@ const ChampionsList = ({ onClickHandler, selectedChampion }) => {
   // change alphabetical order of champion list
   const toggleAtoZ = () => {
     setSortAtoZ(!sortAtoZ);
-    setChampions(prev =>{
+    setChampions((prev) => {
       return [...prev].sort((a, b) => {
-        return sortAtoZ ? b[0].localeCompare(a[0]) : a[0].localeCompare(b[0]) ;
+        return sortAtoZ ? b[0].localeCompare(a[0]) : a[0].localeCompare(b[0]);
       });
-    })
+    });
   };
   // change cost order of champion list
   const toggleCost = () => {
     setSortCost(!sortCost);
-    setChampions(prev=>{
+    setChampions((prev) => {
       return [...prev].sort((a, b) => {
         return sortCost ? a[1] - b[1] : b[1] - a[1];
       });
-    })
+    });
   };
 
+  // display champions as you type in the search bar
+  const searchChampionHandler = (e) => {
+    const typed = e.target.value.toLowerCase();
+    setChampions(() => {
+      return champs.filter((champ) => {
+        return champ[0].toLowerCase().startsWith(typed);
+      });
+    });
+  };
+ 
   return (
     <>
       <div>
@@ -36,12 +47,32 @@ const ChampionsList = ({ onClickHandler, selectedChampion }) => {
           <h1>Champs List</h1>
         </div>
         <div style={{ display: 'flex' }}>
-          <div style={{ cursor: 'pointer' }} onClick={toggleAtoZ}>
-            {sortAtoZ ? 'Z-A' : 'A-Z'}
-          </div>{' '}
-          &nbsp;&nbsp;
-          <div style={{ cursor: 'pointer' }} onClick={toggleCost}>
-            COST
+          <input
+            placeholder="search champion"
+            onChange={(e) => searchChampionHandler(e)}
+          />
+          <div
+            value={sortAtoZ}
+            className="sort"
+            style={{ cursor: 'pointer' }}
+            onClick={toggleAtoZ}
+          >
+            {sortAtoZ ? 'ZtoA' : 'AtoZ'}
+          </div>
+
+          <div
+            className="sort"
+            style={{ cursor: 'pointer' }}
+            onClick={toggleCost}
+          >
+            Cost
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            5<div className="five-indicator" />
+            4<div className="four-indicator" />
+            3<div className="three-indicator" />
+            2<div className="two-indicator" />
+            1<div className="one-indicator" />
           </div>
         </div>
       </div>
@@ -50,7 +81,7 @@ const ChampionsList = ({ onClickHandler, selectedChampion }) => {
           return (
             <div
               key={idx}
-              className={`champion-item blurred-edge ${costArray[champ[1]]} ${
+              className={`champion-item ${costArray[champ[1]]} ${
                 selectedChampion.includes(champ[0]) ? 'selected' : ''
               } `}
               onClick={() => onClickHandler(champ[0])}
