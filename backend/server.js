184a1a087c4711cc312845796bfb43537af09844
ryunkishin <ryunkishin@gmail.com>
2024-01-23 21:06:26 +0900

    cors installation

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+// Enable CORS for all routes
+app.use(cors());
 
 const apiRoutes = require('./routes/apiRoutes')
 
@@ -10,13 +14,28 @@ app.use("/api", apiRoutes);
 const connectDB = require("./config/db");
 connectDB();
 
-// show errors in development mode
+// this is to display error in console for backend
 app.use((error, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     console.error(error);
   }
   next(error);
 });
+
+// show errors in development mode
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    res.status(500).json({
+      message: error.message,
+      // stack may contain sensitive info
+      stack: error.stack,
+    });
+  } else {
+    // production 
+    res.status(500).json({
+      message: error.message,
+    }); 
+}});
 
 const port = 5000
 app.listen(port, ()=>{
